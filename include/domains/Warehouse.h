39c2cebe35309cad8b35ceddc1c54d48ef9cf74b
Gabriel Santos Faria <fariamg4@gmail.com>
2025-06-09 19:18:41 -0300

    feat: implement Warehouse class with ID and packages attributes

---
+++ include/domains/Warehouse.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Stack.h"
+
+class Warehouse {
+private:
+  //@ Atributo identificador do armazém
+  int ID;
+
+  //@ Atributo que armazena os pacotes no armazém na pilha
+  Stack packages;
+
+public:
+    //@ Construtor
+    //TODO: PASSAR AQUI A CAPACIDADE DO ARMAZEM QUE PASSA PARA O CONSTRUTOR DA PILHA INTERNO ( PONTO EXTRA)
+    Warehouse(int ID);
+
+    //@ Função que retorna o ID do armazém
+    int getId() const noexcept;
+
+    //@ Função que retorna a pilha de pacotes do armazém
+    Stack getPackages() const noexcept;
+};
---
+++ src/domains/Warehouse.cpp
@@ -0,0 +1,12 @@
+#include "Warehouse.h"
+
+Warehouse::Warehouse(int ID) : ID(ID) {
+}
+
+int Warehouse::getId() const noexcept {
+    return this->ID;
+}
+
+Stack Warehouse::getPackages() const noexcept {
+    return this->packages;
+}
