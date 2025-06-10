e1138ed01ec4f1efe3494857992fe77f067315da
Gabriel Santos Faria <fariamg4@gmail.com>
2025-06-10 08:53:17 -0300

    refactor: reorganize Warehouse class to encapsulate Session struct and improve code clarity

--- include/domains/Warehouse.h
+++ include/domains/Warehouse.h
@@ -2,23 +2,34 @@
 
 #include "Stack.h"
 
+//@ Classe que implementa o domínio armazem
 class Warehouse {
 private:
+  //@ Struct que define uma sessão de empilhamento de pacotes
+  struct Session {
+    //@ Pilha de pacotes
+    Stack packages;
+
+    //@ Armazem relacionado àquela pilha
+    int relatedWarehouseId;
+  };
+
   //@ Atributo identificador do armazém
   int ID;
 
-  //@ Atributo que armazena os pacotes no armazém na pilha
-  Stack packages;
+  //@ Coleção de sessões de empilhamento de pacotes
+  LinkedList<Session> sessions;
 
 public:
-    //@ Construtor
-    //@ @param ID Identificador único do armazém
-    //TODO: PASSAR AQUI A CAPACIDADE DO ARMAZEM QUE PASSA PARA O CONSTRUTOR DA PILHA INTERNO ( PONTO EXTRA)
-    Warehouse(int ID);
+  //@ Construtor
+  //@ @param ID Identificador único do armazém
+  // TODO: PASSAR AQUI A CAPACIDADE DO ARMAZEM QUE PASSA PARA O CONSTRUTOR DA
+  // PILHA INTERNO ( PONTO EXTRA)
+  Warehouse(int ID);
 
-    //@ Função que retorna o ID do armazém
-    int getId() const noexcept;
+  //@ Função que retorna o ID do armazém
+  int getId() const noexcept;
 
-    //@ Função que retorna a pilha de pacotes do armazém
-    Stack getPackages() const noexcept;
+  //@ Função que retorna a pilha de pacotes do armazém
+  Stack getPackages() const noexcept;
 };
