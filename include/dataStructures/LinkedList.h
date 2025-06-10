e68202437eea634ac0c7c87b01532b3d03e0455d
Gabriel Santos Faria <fariamg4@gmail.com>
2025-06-09 21:25:21 -0300

    refactor: implement LinkedList as a template class and move implementation to header for better usability

--- include/dataStructures/LinkedList.h
+++ include/dataStructures/LinkedList.h
@@ -1,19 +1,21 @@
 #pragma once
 
-#include "Warehouse.h"
+#include <stdexcept> // Para usar std::out_of_range em caso de erro
 
-//@ Classe que implementa uma Linked List para armazenar a rota ótima entre
-//@ armazéns para certo pacote
-class LinkedList {
+//@ Classe que implementa uma lista ligada genérica, para rota ótima e lista de
+//@ pilhas
+template <typename T> class LinkedList {
 private:
-  //@ Struct que define um nó da lista ligada, com seu respectivo
-  //@ armazém e ponteiro para o próximo nó
+  //@ Struct que define um nó da lista ligada
   struct Node {
-    //@ Indice do armazém armazenado no nó
-    int warehouseIndex;
+    //@ Data armazenado no nó
+    T data;
 
     //@ Ponteiro para o próximo nó na lista
     Node *next;
+
+    //@ Construtor do nó que inicializa o dado e o ponteiro para o próximo nó
+    Node(const T &item) : data(item), next(nullptr) {}
   };
 
   //@ Ponteiro para o início e fim da lista
@@ -24,26 +26,61 @@ private:
   int currentSize;
 
 public:
-  //@ Construtor e destrutor
-  LinkedList();
-  ~LinkedList();
+  //@ Construtor
+  LinkedList() {
+    this->head = nullptr;
+    this->tail = nullptr;
+    this->currentSize = 0;
+  }
+
+  //@ Destrutor
+  ~LinkedList() {
+    while (!isEmpty()) {
+      removeFront();
+    }
+  }
 
   //@ Função que retorna o tamanho atual da lista
-  int getCurrentSize() const noexcept;
-
-  //@ Função que remove o primeiro armazém da lista
-  void removeFront();
-
-  //@ Função que retorna o ID do primeiro armazem sem removê-lo
-  int peekFront() const;
-
-  //@ Função que adiciona um nó com o índice do armazém ao final da lista
-  //@ @param warehouseIndex Índice do armazém a ser adicionado
-  void addWarehouse(int& warehouseIndex);
+  int getCurrentSize() const noexcept { return this->currentSize; }
 
   //@ Função que retorna um booleano indicando se a lista está vazia
-  bool isEmpty() const noexcept;
+  bool isEmpty() const noexcept { return this->head == nullptr; }
 
-  //@ Função que limpa a lista, removendo todos os nós
-  // void clear() noexcept;
+  //@ Função que adiciona um novo nó ao final da lista
+  void add(const T &data) {
+    Node *newNode = new Node(data);
+
+    if (isEmpty()) {
+      this->head = newNode;
+      this->tail = newNode;
+    } else {
+      this->tail->next = newNode;
+      this->tail = newNode;
+    }
+    this->currentSize++;
+  }
+
+  //@ Função que remove o primeiro nó da lista
+  void removeFront() {
+    if (isEmpty()) {
+      return;
+    }
+
+    Node *nodeToDelete = this->head;
+    this->head = this->head->next;
+    delete nodeToDelete;
+    this->currentSize--;
+
+    if (isEmpty()) {
+      this->tail = nullptr;
+    }
+  }
+
+  //@ Função que retorna o dado do primeiro nó sem removê-lo
+  T peekFront() const {
+    if (isEmpty()) {
+      throw std::out_of_range("Nao e possivel espiar uma lista vazia.");
+    }
+    return this->head->data;
+  }
 };
