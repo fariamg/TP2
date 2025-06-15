218a292ff0943059d2693a668228640ff3320ce2
Gabriel Santos Faria <fariamg4@gmail.com>
2025-06-15 19:57:03 -0300

    refactor: update Warehouse class to use Session struct for package management

--- src/domains/Warehouse.cpp
+++ src/domains/Warehouse.cpp
@@ -2,7 +2,7 @@
 
 Warehouse::Warehouse(int ID, int totalWarehouses)
     : ID(ID), totalWarehouses(totalWarehouses) {
-  this->sessions = new Stack[totalWarehouses];
+  this->sessions = new Session[totalWarehouses];
 }
 
 Warehouse::~Warehouse() { delete[] this->sessions; }
@@ -11,27 +11,27 @@ int Warehouse::getId() const noexcept { return this->ID; }
 
 void Warehouse::storePackage(const Package *pkg, int destinationId) {
   if (destinationId >= 0 && destinationId < this->totalWarehouses) {
-    this->sessions[destinationId].push(pkg);
+    this->sessions[destinationId].stack.push(pkg);
   }
 }
 
 const Package *Warehouse::retrievePackage(int destinationId) {
   if (destinationId >= 0 && destinationId < this->totalWarehouses) {
-    return this->sessions[destinationId].pop();
+    return this->sessions[destinationId].stack.pop();
   }
   return nullptr;
 }
 
 const Package *Warehouse::peekAtSection(int destinationId) const {
   if (destinationId >= 0 && destinationId < this->totalWarehouses) {
-    return this->sessions[destinationId].peek();
+    return this->sessions[destinationId].stack.peek();
   }
   return nullptr;
 }
 
 bool Warehouse::isSectionEmpty(int destinationId) const {
   if (destinationId >= 0 && destinationId < this->totalWarehouses) {
-    return this->sessions[destinationId].isEmpty();
+    return this->sessions[destinationId].stack.isEmpty();
   }
   return true;
 }
