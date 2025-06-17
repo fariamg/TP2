23d9d4d9dafe43bde8984551c92f0baee5bbbe88
Gabriel Santos Faria <fariamg4@gmail.com>
2025-06-16 21:20:08 -0300

    refactor: update Scheduler constructor and methods for improved event handling and modularity

--- include/core/Scheduler.h
+++ include/core/Scheduler.h
@@ -23,28 +23,34 @@ class Scheduler {
     //@ Construtor que inicializa o escalonador
     // @param maxEvents Capacidade máxima de eventos no escalonador
     //@ @param configData Dados de configuração do sistema
-    Scheduler(int maxEvents = MAX_EVENTS, const IO::ConfigData& configData);
+    Scheduler(const IO::ConfigData& configData, int maxEvents = MAX_EVENTS);
+
+    //@ Função que retorna o próximo evento a ser executado
+    Event* getNextEvent();
 
     //@ Getter que retorna o número total de eventos
     int getTotalEvents() const noexcept;
+
+    //@ Função que retorna o tempo atual do relogio da simulação
+    int getCurrentTime() const noexcept;
 
     //@ Adiciona um novo evento á fila de prioridade
     //@ @event Evento a ser adicionado
     void addEvent(Event* event);
 
-    //@ Função que retorna o próximo evento a ser executado
-    Event* getNextEvent();
-
     //@ Função que remove o próximo evento da fila de prioridade
-    void removeNextEvent();
+    Event* removeNextEvent();
 
     //@ Função que avança o relogio da simulação
     //@ @param timeToAdd Tempo a ser adicinado no timer da simulação
     void advanceTimer(int timeToAdd);
 
-    //@ Função que retorna o tempo atual do relogio da simulação
-    int getCurrentTime() const noexcept;
-
     //@ Função que verifica se ainda há eventos a serem processados
     bool isEmpty() const noexcept;
+
+    void printEvents();
+
+    void updatePreviousStorageTime(Package** packages, int lastIndex, int maxTime);
+
+    void initialStoragesInWarehouse(Warehouse** warehouses) noexcept;
 };
--- src/core/Scheduler.cpp
+++ src/core/Scheduler.cpp
@@ -1,12 +1,15 @@
 #include "../include/core/Scheduler.h"
+#include "../include/ui/OutputLogger.h"
+#include <iostream>
 
-Scheduler::Scheduler(int maxEvents, const IO::ConfigData& configData) : eventsHeap(maxEvents), timer() {
+// TODO: MELHORAR ESSA MODULARIZAÇÃO (RESPONSABILIDADE ÚNICA)
+
+Scheduler::Scheduler(const IO::ConfigData& configData, int maxEvents) : eventsHeap(maxEvents), timer() {
     this->transportCapacity = configData.transportCapacity;
     this->transportLatency = configData.transportLatency;
     this->transportInterval = configData.transportInterval;
     this->removalCost = configData.removalCost;
 
-    //TODO: MELHORAR ESSA MODULARIZAÇÃO (RESPONSABILIDADE ÚNICA)
     for (int i = 0; i < configData.numPackages; ++i) {
         Package* package = configData.packages[i];
         if (package != nullptr) {
@@ -14,36 +17,75 @@ Scheduler::Scheduler(int maxEvents, const IO::ConfigData& configData) : eventsHe
             this->addEvent(event);
         }
     }
-}
 
-int Scheduler::getTotalEvents() const noexcept {
-    return this->eventsHeap.getCurrentSize();
-}
+    timer.setTime(eventsHeap.peekMax()->time); 
 
+    initialStoragesInWarehouse(configData.warehouses);
 
-void Scheduler::addEvent(Event* event) {
-    this->eventsHeap.insert(event);
+    updatePreviousStorageTime(configData.packages, configData.numPackages, timer.getTime());
 }
 
 Event* Scheduler::getNextEvent() {
     return this->eventsHeap.peekMin();
 }
 
-void Scheduler::removeNextEvent() {
+int Scheduler::getTotalEvents() const noexcept {
+    return this->eventsHeap.getCurrentSize();
+}
+
+int Scheduler::getCurrentTime() const noexcept {
+    return this->timer.getTime();
+}
+void Scheduler::addEvent(Event* event) {
+    this->eventsHeap.insert(event);
+}
+
+Event* Scheduler::removeNextEvent() {
     if (this->eventsHeap.isEmpty()) {
         throw std::out_of_range("No events to remove");
     }
-    this->eventsHeap.extractMin();
+    return this->eventsHeap.extractMin();
 }
 
 void Scheduler::advanceTimer(int timeToAdd) {
     this->timer.addTime(timeToAdd);
 }
 
-int Scheduler::getCurrentTime() const noexcept {
-    return this->timer.getTime();
-}
-
 bool Scheduler::isEmpty() const noexcept {
     return this->eventsHeap.isEmpty();
+}
+
+void Scheduler::printEvents() {
+    eventsHeap.printHeap();
+}
+
+void Scheduler::updatePreviousStorageTime(Package** packages, int lastIndex, int maxTime) {
+    for (int i = 0; i < lastIndex; ++i) {
+        if (packages[i] != nullptr) {
+            packages[i]->setTimeStored(maxTime - packages[i]->getPostTime());
+        }
+    }
+}
+
+void Scheduler::initialStoragesInWarehouse(Warehouse** warehouses) noexcept {
+    std::cout << getTotalEvents() << " initial storage events to process." << std::endl;
+    int numEventsToProcess = getTotalEvents();
+
+    for (int i = 0; i < numEventsToProcess; ++i) {
+        Event* event = removeNextEvent();
+
+        Package* package = event->package;
+        package->getRoute().removeFront();
+        int originId = package->getInitialOrigin();
+
+        int nextWarehouseInSectionId = package->getRoute().peekFront();
+
+        warehouses[originId]->storePackage(package, nextWarehouseInSectionId);
+
+        package->getRoute().removeFront();
+
+        package->setCurrentLocation(originId);
+        package->setState(PackageState::STORED);
+        OutputLogger::logPackageStored(package->getPostTime(), package->getId(), originId, nextWarehouseInSectionId);
+    }
 }
